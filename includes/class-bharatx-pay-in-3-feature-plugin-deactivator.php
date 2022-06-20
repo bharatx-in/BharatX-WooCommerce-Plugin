cb617d3a222295e4f170ac1d8eb436d623e17f74
Shyam Murugan <shyam@ip-10-10-8-165.ap-south-1.compute.internal>
2022-06-21 00:44:19 +0530

    version 1.5.1

---
+++ includes/class-bharatx-pay-in-3-feature-plugin-deactivator.php
@@ -0,0 +1,36 @@
+<?php
+
+/**
+ * Fired during plugin deactivation
+ *
+ * @link       https://www.bharatx.tech
+ * @since      1.2.0
+ *
+ * @package    Bharatx_Pay_In_3_Feature_Plugin
+ * @subpackage Bharatx_Pay_In_3_Feature_Plugin/includes
+ */
+
+/**
+ * Fired during plugin deactivation.
+ *
+ * This class defines all code necessary to run during the plugin's deactivation.
+ *
+ * @since      1.2.0
+ * @package    Bharatx_Pay_In_3_Feature_Plugin
+ * @subpackage Bharatx_Pay_In_3_Feature_Plugin/includes
+ * @author     BharatX <Karan@bharatx.tech>
+ */
+class Bharatx_Pay_In_3_Feature_Plugin_Deactivator {
+
+	/**
+	 * Short Description. (use period)
+	 *
+	 * Long Description.
+	 *
+	 * @since    1.2.0
+	 */
+	public static function deactivate() {
+
+	}
+
+}
