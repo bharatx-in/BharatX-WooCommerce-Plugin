f435805256e993584810896ed8fc335470a90f41
Harsh <harshk@bharatx.tech>
2023-09-18 17:14:37 +0000

    saving

--- bharatx-pay-in-3-plugin/bharatx-pay-in-3-feature-plugin.php
+++ bharatx-pay-in-3-plugin/bharatx-pay-in-3-feature-plugin.php
@@ -35,7 +35,7 @@ if ( ! defined( 'WPINC' ) ) {
  * Start at version 1.2.0 and use SemVer - https://semver.org
  * Rename this for your plugin and update it as you release new versions.
  */
-define( 'BHARATX_PAY_IN_3_FEATURE_PLUGIN_VERSION', '1.6.1' );
+define( 'BHARATX_PAY_IN_3_FEATURE_PLUGIN_VERSION', '1.6.2' );
 
 /**
  * Currently plugin slug.
